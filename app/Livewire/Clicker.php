240f3c509b41cc1f2f091d0e81e358c84b9a0845
dieperid <david.dieperink@ik.me>
2024-07-01 10:51:01 +0200

    handling click with livewire

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -6,6 +6,11 @@ use Livewire\Component;
 
 class Clicker extends Component
 {
+    public function handleClick()
+    {
+        dump("Clicked");
+    }
+
     public function render()
     {
         return view('livewire.clicker');
